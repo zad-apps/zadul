1d1e062968fa2b90d6dc03779d191ac4a6d2c52b
zad-apps <nadaesai@gmail.com>
2026-05-05 10:50:19 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'zadul-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './data.json',
+  'https://fonts.googleapis.com/css2?family=Sora:wght@400;600;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(response => {
+        if (e.request.url.includes('/images/')) {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(e.request, response.clone());
+            return response;
+          });
+        }
+        return response;
+      });
+    })
+  );
+});
